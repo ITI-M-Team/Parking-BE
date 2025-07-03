8efd59c64e33587a5e6c4033d28de6f8b5321b6b
Hazemhelal14 <hazemashmawy454@gmail.com>
2025-07-03 11:54:08 +0200

    updating view2

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser  # أضف Garage هنا
+from .models import CustomUser  
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
